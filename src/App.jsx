815658f87d7fc48a89ab95beaef0d300125d450a
Jake Chanda <jake.chanda@gmail.com>
2024-07-20 00:09:30 -0400

    bring files from folder to main project folder

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'semantic-ui-css/semantic.min.css';
+import Background from './Background';
+import { Button, Container, Header, Icon, Image } from 'semantic-ui-react';
+
+function App() {
+
+  return (
+    <>
+      <Background />
+    </>
+  )
+}
+
+export default App
